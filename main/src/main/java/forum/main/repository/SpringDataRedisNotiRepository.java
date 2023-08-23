2e9c242eace9062067c292e8da741ce67feca9ea
season <sionwer4@naver.com>
2023-08-23 13:55:47 +0900

    refactor(main): Delete redis

--- main/src/main/java/forum/main/repository/SpringDataRedisNotiRepository.java
+++
@@ -1,7 +0,0 @@
-package forum.main.repository;
-
-import forum.main.entity.Notification;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SpringDataRedisNotiRepository extends CrudRepository<Notification, Long> {
-}
--- main/src/main/java/forum/main/service/PostService.java
+++ main/src/main/java/forum/main/service/PostService.java
@@ -1,9 +1,7 @@
 package forum.main.service;
 
 import forum.main.dto.NotiMessageDto;
-import forum.main.entity.Notification;
 import forum.main.entity.Post;
-import forum.main.repository.SpringDataRedisNotiRepository;
 import forum.main.service.messaging.KafkaProducer;
 import forum.main.repository.SpringDataDynamoPostRepository;
 import forum.main.repository.SpringDataDynamoTagRepository;
@@ -21,13 +19,11 @@ public class PostService {
 
     private final SpringDataDynamoPostRepository postRepository;
     private final SpringDataDynamoTagRepository tagRepository;
-    private final SpringDataRedisNotiRepository notiRepository;
     private final KafkaProducer producer;
 
-    public PostService(SpringDataDynamoPostRepository repository, SpringDataDynamoTagRepository tagRepository, SpringDataRedisNotiRepository notiRepository, KafkaProducer producer) {
+    public PostService(SpringDataDynamoPostRepository repository, SpringDataDynamoTagRepository tagRepository, KafkaProducer producer) {
         this.postRepository = repository;
         this.tagRepository = tagRepository;
-        this.notiRepository = notiRepository;
         this.producer = producer;
     }
 
@@ -42,12 +38,7 @@ public class PostService {
                 List<Long> userIds = tag.getUserIds();
                 for (Long userId: userIds){
                     if (Objects.equals(userId, writerId)) continue;
-                    Long messageCacheId = userId + postId;
-                    log.info("check: Tag name={}, user id={}, post id={}", tagName, userId, postId);
-                    if (notiRepository.existsById(messageCacheId)) continue;
-                    log.info("confirmed: Tag name = {}, user id = {}, post id = {}", tagName, userId, postId);
                     NotiMessageDto notiMessageDto = new NotiMessageDto(userId, postId);
-                    notiRepository.save(new Notification(messageCacheId));
                     producer.sendMessage(notiMessageDto);
                 }
             });
