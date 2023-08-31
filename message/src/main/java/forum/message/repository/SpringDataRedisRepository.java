b1d5c3489e0718de2222cff810e3da6868291504
season <sionwer4@naver.com>
2023-08-31 16:01:00 +0900

    update git ignore

--- message/src/main/java/forum/message/repository/SpringDataRedisRepository.java
+++
@@ -1,7 +0,0 @@
-package forum.message.repository;
-
-import forum.message.entity.Notification;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SpringDataRedisRepository extends CrudRepository<Notification, Long> {
-}
