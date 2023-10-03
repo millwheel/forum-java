f0ee0c4b279e157a114f845db9e302d783de4fd5
season <sionwer4@naver.com>
2023-10-03 18:03:48 +0900

    refactor(main): Change the properties of user response dto

--- main/src/main/java/forum/main/dto/UserResponseDto.java
+++ main/src/main/java/forum/main/dto/UserResponseDto.java
@@ -7,10 +7,13 @@ import java.util.List;
 
 @Getter
 public class UserResponseDto {
+
+    private Long userId;
     private String username;
     private List<String> tagList;
 
     public UserResponseDto(User user) {
+        this.userId = user.getUserId();
         this.username = user.getUsername();
         this.tagList = user.getTagList();
     }
