807abf00e9ee979d814c83a3281ae49ce19f6635
season <sionwer4@naver.com>
2023-10-03 18:05:44 +0900

    feat(main): Enable scanning of user database(dynamodb)

--- main/src/main/java/forum/main/repository/SpringDataDynamoUserRepository.java
+++ main/src/main/java/forum/main/repository/SpringDataDynamoUserRepository.java
@@ -2,8 +2,10 @@ package forum.main.repository;
 
 import forum.main.entity.User;
 import org.socialsignin.spring.data.dynamodb.repository.DynamoDBCrudRepository;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.stereotype.Repository;
 
+@EnableScan
 @Repository
 public interface SpringDataDynamoUserRepository extends DynamoDBCrudRepository<User, Long> {
 }
